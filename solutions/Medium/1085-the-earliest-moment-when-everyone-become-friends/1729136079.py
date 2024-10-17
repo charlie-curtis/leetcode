a48b24eca4544dda5219f53827c68667e1a56376
Charlie Curtis <charlie-curtis@outlook.com>
2024-10-16 23:34:39 -0400

    Solve 1085. The Earliest Moment When Everyone Become Friends

---
+++ solutions/Medium/1085-the-earliest-moment-when-everyone-become-friends/1729136079.py
@@ -0,0 +1,44 @@
+class DisjointSetUnion:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.num_sets = n
+
+    def find(self, a):
+        acopy = a
+        while a != self.parent[a]:
+            a = self.parent[a]
+        while acopy != a:
+            self.parent[acopy], acopy = a, self.parent[acopy]
+        return a
+
+    def union(self, a, b):
+        a, b = self.find(a), self.find(b)
+        if a != b:
+            if self.size[a] < self.size[b]:
+                a, b = b, a
+
+            self.num_sets -= 1
+            self.parent[b] = a
+            self.size[a] += self.size[b]
+
+    def set_size(self, a):
+        return self.size[self.find(a)]
+
+    def __len__(self):
+        return self.num_sets
+
+class Solution:
+    def earliestAcq(self, logs: List[List[int]], n: int) -> int:
+
+
+        logs.sort()
+        dsu = DisjointSetUnion(n)
+
+        for t, x,y in logs:
+            dsu.union(x,y)
+            if len(dsu) == 1:
+                return t
+        return -1
+
+        
