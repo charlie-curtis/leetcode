6d38abfbabc9e87bc00cbea817e654a02e3b7315
Charlie Curtis <charlie-curtis@outlook.com>
2024-11-16 00:12:40 -0500

    Solve 1404. Print Immutable Linked List In Reverse

---
+++ solutions/Medium/1404-print-immutable-linked-list-in-reverse/1731733960.py
@@ -0,0 +1,21 @@
+# """
+# This is the ImmutableListNode's API interface.
+# You should not implement it, or speculate about its implementation.
+# """
+# class ImmutableListNode:
+#     def printValue(self) -> None: # print the value of this node.
+#     def getNext(self) -> 'ImmutableListNode': # return the next node.
+
+class Solution:
+    def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
+
+
+        def dfs(node):
+            if not node:
+                return
+
+            dfs(node.getNext())
+            node.printValue()
+
+        dfs(head)
+        
