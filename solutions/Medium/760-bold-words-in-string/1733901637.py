17773da2631a480e1866144ec63df064a85bea4a
Charlie Curtis <charlie-curtis@outlook.com>
2024-12-11 02:20:37 -0500

    Solve 760. Bold Words In String

---
+++ solutions/Medium/760-bold-words-in-string/1733901637.py
@@ -0,0 +1,34 @@
+class Solution:
+    def boldWords(self, words: List[str], s: str) -> str:
+
+        d = defaultdict(int)
+
+        n = len(s)
+
+        for i in range(len(words)):
+            word = words[i]
+            m = len(word)
+            for j in range(n-m+1):
+                if word == s[j:j+m]:
+                    d[j]+=1
+                    d[j+m]-=1
+
+        cur = 0
+        isOpen = False
+        out = []
+        for i,x in enumerate(s):
+            cur+=d[i]
+            if not isOpen and cur > 0:
+                out.append("<b>")
+                isOpen = True
+            elif isOpen and cur == 0:
+                out.append("</b>")
+                isOpen = False
+            out.append(x)
+
+        if isOpen:
+            out.append("</b>")
+        
+        return "".join(out)
+
+        
