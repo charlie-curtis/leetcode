a8b35b2911835aae9638da483be2e2c829848f41
Charlie Curtis <charlie-curtis@outlook.com>
2025-01-20 18:12:26 -0500

    Solve 1241. Decompress Run Length Encoded List

---
+++ solutions/Easy/1241-decompress-run-length-encoded-list/1737414746.py
@@ -0,0 +1,11 @@
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+
+        n = len(nums)
+        out = []
+        for i in range(0,n,2):
+            freq = nums[i]
+            val = nums[i+1]
+            out+=freq*[val]
+        return out
+        
