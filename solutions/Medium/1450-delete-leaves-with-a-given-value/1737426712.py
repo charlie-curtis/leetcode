1ba704d03eb9a1cd07f1020a0c61ca782f538cec
Charlie Curtis <charlie-curtis@outlook.com>
2025-01-20 21:31:52 -0500

    Solve 1450. Delete Leaves With A Given Value

---
+++ solutions/Medium/1450-delete-leaves-with-a-given-value/1737426712.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+
+        def dfs(cur):
+            if not cur:
+                return None
+
+            l = dfs(cur.left)
+            r = dfs(cur.right)
+
+            isLeaf = l == None and r == None
+            if cur.val == target and isLeaf:
+                return None
+            else:
+                cur.left = l
+                cur.right = r
+                return cur
+        return dfs(root)
