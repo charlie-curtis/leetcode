ea4e67cdd599f4adaf05039660171fa5ae72ada6
Charlie Curtis <charlie-curtis@outlook.com>
2025-02-03 01:26:37 -0500

    Solve 1704. Special Positions In A Binary Matrix

---
+++ solutions/Easy/1704-special-positions-in-a-binary-matrix/1738563997.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+
+        rows = Counter()
+        cols = Counter()
+        m,n = len(mat), len(mat[0])
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1:
+                    rows[i]+=1
+                    cols[j]+=1
+
+        ans =0
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1 and rows[i] == cols[j] == 1:
+                    ans+=1
+        return ans
