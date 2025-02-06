ce6187e96f5aef15cf8881029ba728ce6726b511
Charlie Curtis <charlie-curtis@outlook.com>
2025-02-05 21:35:25 -0500

    Solve 1720. Crawler Log Folder

---
+++ solutions/Easy/1720-crawler-log-folder/1738809325.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+
+        stack = []
+        for x in logs:
+            if x == '../':
+                if stack:
+                    stack.pop()
+            elif x == './':
+                continue
+            else:
+                stack.append(x)
+
+        return len(stack)
+        
