a43ec9c81acac8ea1738e98e4b86443bfdffb343
Charlie Curtis <charlie-curtis@outlook.com>
2025-07-24 19:20:15 -0400

    Solve 62. Unique Paths

---
+++ solutions/Medium/62-unique-paths/1753399215.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        
+        dp[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                dp[i+1][j]+=dp[i][j]
+                dp[i][j+1]+=dp[i][j]
+        return dp[m-1][n-1]
