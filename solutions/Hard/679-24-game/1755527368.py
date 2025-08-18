bdd7eb97c825d48d045e757c2d14eb572aa3c496
Charlie Curtis <charlie-curtis@outlook.com>
2025-08-18 10:29:28 -0400

    Solve 679. 24 Game

---
+++ solutions/Hard/679-24-game/1755527368.py
@@ -0,0 +1,36 @@
+class Solution:
+    def judgePoint24(self, cards: List[int]) -> bool:
+
+        #brute force - try every permutation of cards. For each permutation, try every combination of operations. For every permutation of cards/combination of operations, try applying them in different orders (to simulate the use of parenthesis)
+        def check(cards):
+            if len(cards) == 1:
+                #handle small rounding errors using a tolerance
+                return abs(cards[0] - 24) < .0005
+
+            for op in ['-', '+', '*', '/']:
+                for i in range(len(cards)-1):
+                    if op == '-':
+                        R = cards[i] - cards[i+1]
+                    elif op == '+':
+                        R = cards[i] + cards[i+1]
+                        pass
+                    elif op == '*':
+                        R = cards[i] * cards[i+1]
+                        pass
+                    else:
+                        if cards[i+1] == 0:
+                            continue
+                        R = cards[i] / cards[i+1]
+                    
+                    new = cards[:i] + [R]
+                    if i+2 < len(cards):
+                        new+=cards[i+2:]
+                    if check(new):
+                        return True
+            return False
+
+
+        for card_perms in permutations(cards):
+            if check(list(card_perms)):
+                return True
+        return False
