e3cbef8ad15037abb74bf5f939ca9af2b6c594fb
Charlie Curtis <charlie-curtis@outlook.com>
2025-08-23 21:15:47 -0400

    Solve 201. Bitwise And Of Numbers Range

---
+++ solutions/Medium/201-bitwise-and-of-numbers-range/1755998147.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rangeBitwiseAnd(self, low: int, high: int) -> int:
+
+        out = 0
+        #iterate through all the bits of LOW. If a bit is set to 1, find the next number that is greater than LOW that has this bit set to 0. If that number is less than HIGH, then we know that the bit will be a 0 in the final answer
+        for i in range(32):
+            x = 1<<i
+            if x & low:
+                #this bit is currently set, zero out all lte this one and add x*2
+                y = low
+                for j in range(i+1):
+                    if low&(1<<j):
+                        y^=(1<<j)
+                y+=2*x
+                if y > high:
+                    out|=x
+        return out
+
+
+
+        
