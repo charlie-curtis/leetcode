042348e1e91a48b49a89fad72da3017274651602
Charlie Curtis <charlie-curtis@outlook.com>
2025-08-23 22:33:16 -0400

    Solve 201. Bitwise And Of Numbers Range

--- solutions/Medium/201-bitwise-and-of-numbers-range/1756002796.py
+++ solutions/Medium/201-bitwise-and-of-numbers-range/1756002796.py
@@ -7,10 +7,7 @@ class Solution:
             x = 1<<i
             if x & low:
                 #this bit is currently set, zero out all lte this one and add x*2
-                y = low
-                for j in range(i+1):
-                    if low&(1<<j):
-                        y^=(1<<j)
+                y = (low >> (i+1)) << (i+1)
                 y+=2*x
                 if y > high:
                     out|=x
