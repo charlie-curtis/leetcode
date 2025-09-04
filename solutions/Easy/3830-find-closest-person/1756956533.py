70b25d06652d2935bcc5cbfb0ec103dc427585e1
Charlie Curtis <charlie-curtis@outlook.com>
2025-09-03 23:28:53 -0400

    Solve 3830. Find Closest Person

---
+++ solutions/Easy/3830-find-closest-person/1756956533.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+
+        a = abs(x-z)
+        b = abs(y-z)
+
+        if a < b:
+            return 1
+        if a > b:
+            return 2
+        return 0
+        
